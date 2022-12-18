93ba0e0e0b3a33a7e51620f41bfd2e2b52f68e69
imotai <codego.me@gmail.com>
2022-12-18 22:26:11 +0800

    fix: add collection and doc

--- src/components/NamespaceDetail.component.tsx
+++ src/components/NamespaceDetail.component.tsx
@@ -1,35 +1,76 @@
-import React, { memo, useCallback, useEffect, useState } from 'react'
+import React, {useMemo, memo, useCallback, useEffect, useState } from 'react'
 import { Button, Form, Input, Modal, Tabs, Typography } from 'antd'
+import { DB3, sign,DocMetaManager,DocStore, DocKeyType} from "db3js";
 import { PlusCircleOutlined } from '@ant-design/icons'
-
+import { useRecoilValue } from "recoil";
 import '../styles/namespaceDetail.scss'
 import { useParams } from 'react-router-dom'
 import { useAsyncFn } from 'react-use'
 import { divide } from 'lodash'
+import { publicKeyAtom, secretAtom } from "../state";
+import { decode } from "uint8-to-base64";
+import { Buffer as BufferPolyfill } from 'buffer'
+globalThis.Buffer = BufferPolyfill;
 
 const { Title } = Typography
 
 const Collections: React.FC<{}> = memo((props) => {
+    const sk = useRecoilValue(secretAtom);
+	const pk = useRecoilValue(publicKeyAtom);
+	const db3_instance = useMemo(() => new DB3("http://127.0.0.1:26659"), []);
     const { name: ns_name } = useParams()
     const [visible, setVisible] = useState(false)
     const [docVisible, setDocVisible] = useState(false)
-    const [hash, addCollection] = useAsyncFn(async (collection_name, doc_index) => {
+    const [cname, addCollection] = useAsyncFn(async (collection_name, doc_index) => {
+        async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
+			return [await sign(data, decode(sk)), decode(pk)];
+		} 
+        const keys = [];
+        const doc_index_obj = JSON.parse(doc_index);
+        doc_index_obj.keys.forEach((key) => {
+            const new_key = {name: key.name,
+            keyType:null};
+            if (key.keyType == "string") {
+                new_key.keyType = DocKeyType.STRING;
+            }else {
+                new_key.keyType = DocKeyType.NUMBER; 
+            }
+            keys.push(new_key);
+        });
+        const new_doc_index = {
+            keys:keys,
+            docName:collection_name,
+            ns:ns_name
+        };
+        const doc_meta_mgr = new DocMetaManager(db3_instance);
+        try {
+            const result = await doc_meta_mgr.create_doc_meta(new_doc_index, "desc", _sign);
+            console.log(result);
+        } catch (error) {
+            console.log(error);
+        }
         // create doc_meta
-        setVisible(false)
-        return 'fdsfsdfsdfd'
-    }, [])
+        setVisible(false);
+        return collection_name;
+    }, [db3_instance, ns_name])
     const [docMetasState, getAllDocMetas] = useAsyncFn(async () => {
-        console.log('getAllDocMetas')
-        return [
-            { doc_name: 'test1', index: {} },
-            { doc_name: 'test2', index: {} },
-        ]
-    }, [hash])
+        async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
+			return [await sign(data, decode(sk)), decode(pk)];
+		} 
+        const doc_meta_mgr = new DocMetaManager(db3_instance);
+        const result = await doc_meta_mgr.get_all_doc_metas(ns_name, _sign);
+        return result;
+    }, [db3_instance, ns_name])
     const [docs, getDocs] = useAsyncFn(
         async (doc_index) => {
-            return [{ key: 'xxxx', data: { name: '123' } }]
+            async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
+                return [await sign(data, decode(sk)), decode(pk)];
+            } 
+            const doc_store = new DocStore(db3_instance);
+            const docs = await doc_store.queryAllDocs(ns_name, doc_index, _sign);
+            return docs;
         },
-        [hash]
+        [db3_instance, ns_name]
     )
 
     const [doc, getDocByKey] = useAsyncFn(
@@ -38,16 +79,32 @@ const Collections: React.FC<{}> = memo((props) => {
         },
         [docs]
     )
+
     const [docsInfo, setDocsInfo] = useState('')
-    const [insertDocHash, insertDocs] = useAsyncFn(async () => {
-        // insertDocs
-        setDocVisible(false)
-        console.log(docsInfo)
-    }, [docsInfo])
+    const [insertDocHash, insertDocs] = useAsyncFn(async (name, doc_content) => {
+        async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
+            return [await sign(data, decode(sk)), decode(pk)];
+        } 
+        try {
+            const doc_index = docMetasState.value?.find((item) => item.doc_name === name)?.index
+            const doc_store = new DocStore(db3_instance)
+            const hash = await doc_store.insertDocs(doc_index, [JSON.parse(doc_content)], _sign)
+            setDocVisible(false)
+            return hash
+        } catch(error) {
+            console.log(error)
+        }
+    }, [docMetasState, db3_instance, ns_name])
     function onFinish(values: any) {
         const { name, index } = values
         addCollection(name, index)
     }
+
+    function onAddDocFinish(values: any) {
+        const { name, doc_content } = values
+        insertDocs(name, doc_content)
+    }
+
     useEffect(() => {
         getAllDocMetas()
     }, [])
@@ -76,11 +133,9 @@ const Collections: React.FC<{}> = memo((props) => {
                     </div>
                     {docs.value?.map((item) => (
                         <div
-                            key={item.key}
-                            onClick={() => getDocByKey(item.key)}
                             style={{ padding: '7px 10px', cursor: 'pointer' }}
                         >
-                            {item.key}
+                            {JSON.stringify(item)}
                         </div>
                     ))}
                 </div>
@@ -126,13 +181,39 @@ const Collections: React.FC<{}> = memo((props) => {
             <Modal
                 title="Add doc"
                 open={docVisible}
-                onOk={insertDocs}
                 onCancel={() => setDocVisible(false)}
+                footer={null}
             >
-                <Input.TextArea
-                    value={docsInfo}
-                    onChange={(e) => setDocsInfo(e.target.value)}
-                ></Input.TextArea>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onAddDocFinish}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Collection Name"
+                        name="name"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Doc Content"
+                        name="doc_content"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.TextArea />
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
             </Modal>
         </>
     )
