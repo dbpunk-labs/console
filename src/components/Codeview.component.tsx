87efc49d3591357f77dbb78d3c9837051a5edfe9
zhaojun <zhaojunbest@gmail.com>
2022-12-19 14:03:34 +0800

    dev codeview

---
+++ src/components/Codeview.component.tsx
@@ -0,0 +1,88 @@
+import React, { memo, useMemo, useState } from 'react'
+import { Button, Select, Space, Typography } from 'antd'
+import { useParams } from 'react-router-dom'
+import Editor from '@monaco-editor/react'
+import { DB3, DocStore, DocKeyType, sign, DocMetaManager } from 'db3js'
+import { defaultCode } from './defaultCode'
+import { useRecoilState } from 'recoil'
+import { publicKeyAtom, secretAtom } from '../state'
+import { decode } from 'uint8-to-base64'
+import { useAsync } from 'react-use'
+
+const { Title, Text } = Typography
+const { Option } = Select
+
+const CodeView: React.FC<{}> = memo((props) => {
+    const [result, setResult] = useState<string>()
+    const console = {
+        log: function (value: Record<string, any>) {
+            setResult(JSON.stringify(value))
+        },
+    }
+    const { name: ns_name } = useParams()
+    const db3_instance = useMemo(() => new DB3('http://127.0.0.1:26659'), [])
+    const doc_store = useMemo(() => new DocStore(db3_instance), [db3_instance])
+    const [code, setCode] = useState<string>(defaultCode)
+    const [sk] = useRecoilState(secretAtom)
+    const [pk] = useRecoilState(publicKeyAtom)
+    async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
+        return [await sign(data, decode(sk)), decode(pk)]
+    }
+    const docMetasState = useAsync(async () => {
+        const doc_meta_mgr = new DocMetaManager(db3_instance)
+        try {
+            const result = await doc_meta_mgr.get_all_doc_metas(ns_name, _sign)
+            return result
+        } catch (error) {
+            console.error(error)
+        }
+    }, [db3_instance, ns_name])
+    const [docName, setDocName] = useState<string>()
+    function runInsertDocCode() {
+        const fn = new Function('db3_instance', 'doc_store', 'DocKeyType', '_sign', 'docName', code)
+        fn(db3_instance, doc_store, DocKeyType, _sign, docName)
+    }
+    return (
+        <div className="code-view">
+            <Space>
+                <Title level={3}>NS: {ns_name}</Title>
+                <div style={{ display: 'flex' }}>
+                    collection:
+                    <Select
+                        value={docName}
+                        onChange={(value) => setDocName(value)}
+                        style={{ width: 300 }}
+                    >
+                        {docMetasState.value?.map((item) => (
+                            <Option value={item.doc_name}>{item.doc_name}</Option>
+                        ))}
+                    </Select>
+                </div>
+            </Space>
+            <div>
+                <Text>
+                    This table gives the most recent erc-20 token balance of an address. The data
+                    refreshes every ETH block or 13s
+                </Text>
+            </div>
+            <div>
+                <Title level={5}>insert data</Title>
+            </div>
+            <div>
+                <Text>Only table owner have the right to write table</Text>
+            </div>
+            <Editor
+                theme="vs-dark"
+                height="30vh"
+                defaultLanguage="javascript"
+                value={code}
+                onChange={(value) => setCode(value)}
+            />
+            <Button type="primary" onClick={runInsertDocCode}>
+                Run
+            </Button>
+            <div>{result}</div>
+        </div>
+    )
+})
+export default CodeView
