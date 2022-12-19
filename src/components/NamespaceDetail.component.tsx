3395a539d1515b732596ad88fdad286a1b52b111
zhaojun <zhaojunbest@gmail.com>
2022-12-19 14:46:09 +0800

    dev codeview

--- src/components/NamespaceDetail.component.tsx
+++ src/components/NamespaceDetail.component.tsx
@@ -1,74 +1,76 @@
-import React, {useMemo, memo, useCallback, useEffect, useState } from 'react'
+import React, { useMemo, memo, useCallback, useEffect, useState } from 'react'
 import { Button, Form, Input, Modal, Tabs, Typography } from 'antd'
-import { DB3, sign,DocMetaManager,DocStore, DocKeyType} from "db3js";
+import { DB3, sign, DocMetaManager, DocStore, DocKeyType } from 'db3js'
 import { PlusCircleOutlined } from '@ant-design/icons'
-import { useRecoilValue } from "recoil";
+import { useRecoilValue } from 'recoil'
 import '../styles/namespaceDetail.scss'
 import { useParams } from 'react-router-dom'
 import { useAsyncFn } from 'react-use'
-import { divide } from 'lodash'
-import { publicKeyAtom, secretAtom } from "../state";
-import { decode } from "uint8-to-base64";
+import { publicKeyAtom, secretAtom } from '../state'
+import { decode } from 'uint8-to-base64'
 import { Buffer as BufferPolyfill } from 'buffer'
-globalThis.Buffer = BufferPolyfill;
+import CodeView from './Codeview.component'
+globalThis.Buffer = BufferPolyfill
 
 const { Title } = Typography
 
 const Collections: React.FC<{}> = memo((props) => {
-    const sk = useRecoilValue(secretAtom);
-	const pk = useRecoilValue(publicKeyAtom);
-	const db3_instance = useMemo(() => new DB3("http://127.0.0.1:26659"), []);
+    const sk = useRecoilValue(secretAtom)
+    const pk = useRecoilValue(publicKeyAtom)
+    const db3_instance = useMemo(() => new DB3('http://127.0.0.1:26659'), [])
     const { name: ns_name } = useParams()
     const [visible, setVisible] = useState(false)
     const [docVisible, setDocVisible] = useState(false)
-    const [cname, addCollection] = useAsyncFn(async (collection_name, doc_index) => {
-        async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
-			return [await sign(data, decode(sk)), decode(pk)];
-		} 
-        const keys = [];
-        const doc_index_obj = JSON.parse(doc_index);
-        doc_index_obj.keys.forEach((key) => {
-            const new_key = {name: key.name,
-            keyType:null};
-            if (key.keyType == "string") {
-                new_key.keyType = DocKeyType.STRING;
-            }else {
-                new_key.keyType = DocKeyType.NUMBER; 
+    const [cname, addCollection] = useAsyncFn(
+        async (collection_name, doc_index) => {
+            async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
+                return [await sign(data, decode(sk)), decode(pk)]
             }
-            keys.push(new_key);
-        });
-        const new_doc_index = {
-            keys:keys,
-            docName:collection_name,
-            ns:ns_name
-        };
-        const doc_meta_mgr = new DocMetaManager(db3_instance);
-        try {
-            const result = await doc_meta_mgr.create_doc_meta(new_doc_index, "desc", _sign);
-            console.log(result);
-        } catch (error) {
-            console.log(error);
-        }
-        // create doc_meta
-        setVisible(false);
-        return collection_name;
-    }, [db3_instance, ns_name])
+            const keys = []
+            const doc_index_obj = JSON.parse(doc_index)
+            doc_index_obj.keys.forEach((key) => {
+                const new_key = { name: key.name, keyType: null }
+                if (key.keyType == 'string') {
+                    new_key.keyType = DocKeyType.STRING
+                } else {
+                    new_key.keyType = DocKeyType.NUMBER
+                }
+                keys.push(new_key)
+            })
+            const new_doc_index = {
+                keys: keys,
+                docName: collection_name,
+                ns: ns_name,
+            }
+            const doc_meta_mgr = new DocMetaManager(db3_instance)
+            try {
+                const result = await doc_meta_mgr.create_doc_meta(new_doc_index, 'desc', _sign)
+                console.log(result)
+            } catch (error) {
+                console.log(error)
+            }
+            // create doc_meta
+            setVisible(false)
+            return collection_name
+        },
+        [db3_instance, ns_name]
+    )
     const [docMetasState, getAllDocMetas] = useAsyncFn(async () => {
         async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
-			return [await sign(data, decode(sk)), decode(pk)];
-		} 
-        const doc_meta_mgr = new DocMetaManager(db3_instance);
-        const result = await doc_meta_mgr.get_all_doc_metas(ns_name, _sign);
-        return result;
+            return [await sign(data, decode(sk)), decode(pk)]
+        }
+        const doc_meta_mgr = new DocMetaManager(db3_instance)
+        const result = await doc_meta_mgr.get_all_doc_metas(ns_name, _sign)
+        return result
     }, [db3_instance, ns_name])
     const [docs, getDocs] = useAsyncFn(
         async (doc_index) => {
             async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
-                return [await sign(data, decode(sk)), decode(pk)];
-            } 
-            const doc_store = new DocStore(db3_instance);
-            const docs = await doc_store.queryAllDocs(ns_name, doc_index, _sign);
-            return docs;
+                return [await sign(data, decode(sk)), decode(pk)]
+            }
+            const doc_store = new DocStore(db3_instance)
+            const docs = await doc_store.queryAllDocs(ns_name, doc_index, _sign)
+            return docs
         },
         [db3_instance, ns_name]
     )
@@ -81,20 +83,23 @@ const Collections: React.FC<{}> = memo((props) => {
     )
 
     const [docsInfo, setDocsInfo] = useState('')
-    const [insertDocHash, insertDocs] = useAsyncFn(async (name, doc_content) => {
-        async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
-            return [await sign(data, decode(sk)), decode(pk)];
-        } 
-        try {
-            const doc_index = docMetasState.value?.find((item) => item.doc_name === name)?.index
-            const doc_store = new DocStore(db3_instance)
-            const hash = await doc_store.insertDocs(doc_index, [JSON.parse(doc_content)], _sign)
-            setDocVisible(false)
-            return hash
-        } catch(error) {
-            console.log(error)
-        }
-    }, [docMetasState, db3_instance, ns_name])
+    const [insertDocHash, insertDocs] = useAsyncFn(
+        async (name, doc_content) => {
+            async function _sign(data: Uint8Array): Promise<[Uint8Array, Uint8Array]> {
+                return [await sign(data, decode(sk)), decode(pk)]
+            }
+            try {
+                const doc_index = docMetasState.value?.find((item) => item.doc_name === name)?.index
+                const doc_store = new DocStore(db3_instance)
+                const hash = await doc_store.insertDocs(doc_index, [JSON.parse(doc_content)], _sign)
+                setDocVisible(false)
+                return hash
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        [docMetasState, db3_instance, ns_name]
+    )
     function onFinish(values: any) {
         const { name, index } = values
         addCollection(name, index)
@@ -134,9 +139,7 @@ const Collections: React.FC<{}> = memo((props) => {
                         Add doc
                     </div>
                     {docs.value?.map((item) => (
-                        <div
-                            style={{ padding: '7px 10px', cursor: 'pointer' }}
-                        >
+                        <div style={{ padding: '7px 10px', cursor: 'pointer' }}>
                             {JSON.stringify(item)}
                         </div>
                     ))}
@@ -221,10 +224,6 @@ const Collections: React.FC<{}> = memo((props) => {
     )
 })
 
-const Detail: React.FC<{}> = memo((props) => {
-    return <div className="ns-detail"></div>
-})
-
 const NamespaceDetail: React.FC<{}> = memo((props) => {
     const { name: ns_name } = useParams()
     return (
@@ -242,7 +241,7 @@ const NamespaceDetail: React.FC<{}> = memo((props) => {
                     {
                         label: `Detail`,
                         key: '3',
-                        children: <Detail />,
+                        children: <CodeView />,
                     },
                 ]}
             ></Tabs>
